a7b3968c3d64cb420294d5afc44bd4a5a523142b
Animesh <animesh.rastogi@searce.com>
2022-09-01 03:37:16 +0000

    Changed Version to v1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8080;
 
 // Get Route for Root Path
 app.get("/", (req, res)=>{
-    res.send("<h1>Hello from Node App v2</h1>");
+    res.send("<h1>Hello from Node App v1</h1>");
 });
 
 // Server Listening to requests on PORT 8080
